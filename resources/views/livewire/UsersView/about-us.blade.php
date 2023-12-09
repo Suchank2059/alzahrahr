83dadb7addca4bc12bd11803d1bbb34a36cae8e1
rupesh-2 <132558374+rupesh-2@users.noreply.github.com>
2023-12-10 04:36:53 +0545

    homepage made

--- resources/views/livewire/UsersView/about-us.blade.php
+++ resources/views/livewire/UsersView/about-us.blade.php
@@ -1,4 +1,5 @@
 <div>
     <h1>This is About us page</h1>
+
     <livewire:navigation />
 </div>
--- resources/views/livewire/UsersView/services.blade.php
+++ resources/views/livewire/UsersView/services.blade.php
@@ -1,4 +1,21 @@
-<div>
-    <h1>This is services pages</h1>
-    <livewire:navigation />
+<div class="h-screen w-screen bg-gray-200 ">
+    <div class="w-screen flex items-center justify-center font-serif text-emerald-900">
+        <img class="mr-4" src="image/logo%20zahra.png">
+        <div class="leading-3">
+            <h1 class="text-4xl">Al Zahara</h1>
+            <p class="flex justify-center">Human Resources Consulttancy</p>
+        </div>
+    </div>
+    <div class="border-b border-green-200-200 w-screen  my-4"></div>
+    <livewire:navigation/>
+    <div class="border-b border-green-200-200 w-screen  my-4"></div>
+    <div class=" flex justify-center items-center">
+        <p class="text-2xl text-emerald-800 font-light">Our Services</p>
+    </div>
+    <div class="px-16 py-6 h-8 bg-white my-4">
+       <p class="text-xl text-emerald-700 font-extralight">
+            Recruiment Services
+       </p> 
+    </div>
+    
 </div>
