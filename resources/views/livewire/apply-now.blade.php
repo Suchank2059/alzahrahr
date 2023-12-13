28182abb84d10202b6d6720e5cb53b924ad4df5e
rupesh-2 <132558374+rupesh-2@users.noreply.github.com>
2023-12-13 10:56:16 +0545

    apply now form complete

--- resources/views/livewire/apply-now.blade.php
+++ resources/views/livewire/apply-now.blade.php
@@ -1,44 +1,48 @@
-<div>
-    <div class="w-screen  px-64 flex justify-center">
-        <form action="#" method="post" enctype="multipart/form-data">
-            <!-- Input Fields -->
-            <div class="flex">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required><br>
+<div class="bg-gray-100  ">
+    <livewire:navigation />
+    <livewire:web-livewire />
+    <div class="flex justify-center">
+    <form class=" xl:flex xl:max-w-md bg-white flex items-center justify-center mt-32 p-6 rounded-md shadow-md">
+        <div class="flex items-center ">
+            <div>
+        <label class="block mb-2" for="name">Name:</label>
+        <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-md" required>
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required><br>
+        <label class="block mb-2" for="email">Email:</label>
+        <input type="email" id="email" name="email" class="w-full px-4 py-2 border rounded-md" required>
 
-            <label for="age">Age:</label>
-            <input type="number" id="age" name="age" required><br>
+        <label class="block mb-2" for="phone">Phone:</label>
+        <input type="number" maxlength="10" id="phone" name="phone" class="w-full px-4 py-2 border rounded-md" pattern="[0-9]{10}" title="Please enter a 10-digit numeric phone number" required>
 
-            <label for="gender">Gender:</label>
-            <select id="gender" name="gender" required>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-            </select><br>
 
-            <label for="phone">Phone:</label>
-            <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required><br>
+        <label class="block mb-2" for="subject">Subject:</label>
+        <input type="text" id="subject" name="subject" class="w-full px-4 py-2 border rounded-md" required>
+
+        <label class="block mb-2" for="age">Age:</label>
+        <input type="number" id="age" name="age" class="w-full px-4 py-2 border rounded-md" required>
+
+        <label class="block mb-2" for="message">Message:</label>
+        <textarea id="message" name="message" class="w-full px-4 py-2 border rounded-md resize-y" required></textarea>
+
+        <div class="mb-4">
+            <label class="block mb-2" for="file1">Upload Passport</label>
+            <input type="file" id="file1" name="file1" accept=".jpg, .jpeg, .png" class="w-full">
+        </div>
+
+        <div class="mb-4">
+            <label class="block mb-2 rounded-xl" for="file2">Upload C.V</label>
+            <input type="file" id="file2" name="file2" accept=".pdf" class="w-full ">
+        </div>
+
+        <div class="mb-4">
+            <label class="block mb-2" for="file3">Latest Photo</label>
+            <input type="file" id="file3" name="file3" accept=".doc, .docx" class="w-full">
+        </div>
+
+        <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">Submit</button>
             </div>
-            <!-- Message Area -->
-            <label for="message">Message:</label><br>
-            <textarea id="message" name="message" rows="4" cols="50" required></textarea><br>
-
-            <!-- Upload Fields -->
-            <label for="file1">Upload File 1:</label>
-            <input type="file" id="file1" name="file1" accept=".jpg, .jpeg, .png"><br>
-
-            <label for="file2">Upload File 2:</label>
-            <input type="file" id="file2" name="file2" accept=".pdf"><br>
-
-            <label for="file3">Upload File 3:</label>
-            <input type="file" id="file3" name="file3" accept=".doc, .docx"><br>
-
-            <!-- Submit Button -->
-            <input type="submit" value="Submit">
-        </form>
-
+            </div>
+    </form>
     </div>
+    <livewire:footer />
 </div>
--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,11 +1,14 @@
 <div>
     <header class=" lg:pt-6 lg:pb-14 py-8 ">
         <div class="container mx-auto lg:relative flex flex-col lg:flex-row lg:justify-between gap-y-4 lg:gap-0">
-            <div class="flex justify-center lg:justify-normal">
-                <a href="#">
-                    <img alt="error" src="image/logo%20zahra.png">
-                </a>
+            <div class="w-screen flex  font-serif text-emerald-900">
+                <img class="mr-4" src="image/logo%20zahra.png">
+                <div class="leading-3">
+                    <h1 class="text-4xl">Al Zahara</h1>
+                    <p class="flex justify-center">Human Resources Consulttancy</p>
+                </div>
             </div>
+
             <div class="flex flex-col gap-y-4 lg:flex-row lg:gap-x-10 lg:gap-y-0">
                 <div  class="flex justify-center items-center gap-x-2 lg:justify-normal text-secondary">
                     <i class="fa fa-map-marker" aria-hidden="true">
