3a916479d7aabcc4f92424651f3cab38497ff5d2
Suchank2059 <132805306+Suchank2059@users.noreply.github.com>
2023-12-28 22:39:29 +0545

    implement database in ui

---
+++ Services/GetCompanyProfile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Services;
+
+use App\Models\CompanyProfile;
+use Ramsey\Uuid\Type\Integer;
+
+class GetCompanyProfile
+{
+    public function getCompanyLocation(): string
+    {
+        $location = CompanyProfile::where('email', 'alzahranepal@gmail.com')->get('location');
+        return $location;
+    }
+    public function getCompanyContact(): Integer
+    {
+        $contact = CompanyProfile::where('email', 'alzahranepal@gmail.com')->get('primary_contact');
+        return $contact;
+    }
+}
--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -3,24 +3,32 @@
     <footer class="footer pt-12 xl:pt-[50px] mt-auto">
         <div class="container mx-auto flex flex-wrap justify-between">
             <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 mb-6">
-                <h3 class="text-lg font-bold mb-3">Office 1</h3>
-                <p>Cityville, State 12345</p>
+                <h3 class="text-lg font-bold mb-3">Dubai Office</h3>
+                <p><i class="fa fa-map-marker mr-2"></i><span class="text-accent"> Tower, Business Bay, Dubai United Arab
+                        Emirates</span></p>
+                <p><i class="fa fa-phone mr-2"></i><span class="text-accent">+9715599976543</span></p>
+                <p> <i class="fa fa-envelope mr-2"></i><span class="text-accent">@alzahrahr.com</span></p>
+
             </div>
             <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 mb-6">
-                <h3 class="text-lg font-bold mb-3">Office 2</h3>
-                <p><i class="fa fa-map-marker mr-2"></i>456 Oak Avenue</p>
-                <p>Townsville, State 67890</p>
+                <h3 class="text-lg font-bold mb-3">Nepal Office</h3>
+                <p><i class="fa fa-map-marker mr-2"></i><span class="text-accent"> {{ $location }}</span></p>
             </div>
             <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 mb-6">
                 <h3 class="text-lg font-bold mb-3">Contact Us</h3>
-                <p><i class="fa fa-envelope mr-2"></i>Email: {{ $email }}</p>
-                <p><i class="fa fa-phone mr-2"></i>Phone: {{ $primary_contact }}</p>
+                <p><i class="fa fa-envelope mr-2"></i>Email: <span class="text-accent">{{ $email }}</span></p>
+                <p><i class="fa fa-phone mr-2"></i>Primary Contact: <span
+                        class="text-accent">{{ $primaryContact }}</span></p>
+                <p><i class="fa fa-phone mr-2"></i>Secondary Contact : <span
+                        class="text-accent">{{ $secondaryContact }}</span></p>
+
             </div>
             <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 mb-6">
                 <h3 class="text-lg font-bold mb-3">Map</h3>
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.380540846576!2d85.32091627561093!3d27.70553457618367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1973c96e0cd5%3A0xdbb28c517dd06bfb!2sEmperor%20Education%20Network!5e0!3m2!1sen!2snp!4v1688623817693!5m2!1sen!2snp"
-                    height="200" class="w-full">
+                    src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d182533.8656444197!2d85.1851941130638!3d27.679673560011242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d27.6453743!2d85.253795!4m5!1s0x39eb190025b96a67%3A0xbf2daabad513b674!2sP82V%2B5FH%2C%20Kathmandu%2044600!3m2!1d27.700432!2d85.3437495!5e0!3m2!1sen!2snp!4v1703779837809!5m2!1sen!2snp"
+                    width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
             </div>
         </div>
